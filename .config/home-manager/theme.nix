546bb07c5b3e40e240ccfd9b8e402230d94bae93
Christian Smith <smith.christian.william@gmail.com>
2023-08-26 07:52:31 -0400

    fix cursor bug

--- .config/home-manager/theme.nix
+++ .config/home-manager/theme.nix
@@ -1,5 +1,15 @@
 { pkgs, userHome, ... }:
 {
+  home.pointerCursor = {
+    package = pkgs.gnome.adwaita-icon-theme;
+    name = "Adwaita";
+    size = 40;
+    gtk.enable = true;
+    x11 = {
+      enable = true;
+      defaultCursor = "left_ptr";
+    };
+  };
   fonts.fontconfig.enable = true;
   gtk.enable = true;
   gtk.font = {
@@ -7,9 +17,6 @@
     name = "Noto Sans";
     size = 11;
   };
-  gtk.cursorTheme.package = pkgs.gnome.adwaita-icon-theme;
-  gtk.cursorTheme.name = "Adwaita";
-  gtk.cursorTheme.size = 40;
   gtk.theme.package = pkgs.adw-gtk3;
   gtk.theme.name = "adw-gtk3-dark";
   gtk.iconTheme.package = pkgs.papirus-icon-theme;
