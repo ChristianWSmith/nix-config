17d87d21971d92130b00902a77408bea279d1fb6
Christian Smith <smith.christian.william@gmail.com>
2023-08-29 16:12:37 -0400

    cleanup

--- .config/home-manager/theme.nix
+++ .config/home-manager/theme.nix
@@ -1,8 +1,6 @@
 { pkgs, userHome, iconTheme, font, ... }:
 {
   home.pointerCursor = {
-    # package = pkgs.gnome.adwaita-icon-theme;
-    # name = "Adwaita";
     package = pkgs.capitaine-cursors;
     name = "capitaine-cursors";
     size = 40;
@@ -19,8 +17,6 @@
     name = "${font}";
     size = 11;
   };
-  # gtk.theme.package = pkgs.adw-gtk3;
-  # gtk.theme.name = "adw-gtk3-dark";
   gtk.theme.package = pkgs.whitesur-gtk-theme;
   gtk.theme.name = "WhiteSur-Dark";
   gtk.iconTheme.package = pkgs.whitesur-icon-theme;
@@ -35,6 +31,5 @@
   ];
   qt.enable = true;
   qt.platformTheme = "gtk";
-  # qt.style.name = "adwaita-dark";
   qt.style.name = "WhiteSur-Dark";
 }
