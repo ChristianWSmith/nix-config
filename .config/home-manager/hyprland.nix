a970c37eae27035a52d57182ee95aa0730cf7f8b
Christian Smith <smith.christian.william@gmail.com>
2023-08-29 16:24:05 -0400

    more todo cleanup

--- .config/home-manager/hyprland.nix
+++ .config/home-manager/hyprland.nix
@@ -2,35 +2,10 @@
 let
   launcher = pkgs.writeShellScriptBin "hyprland-launcher" ''
     . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-
-    # TODO: DO THESE DO ANYTHING??
-
-    # export GDK_BACKEND=wayland,x11
-    # export QT_QPA_PLATFORM="wayland;xcb"
-    # export SDL_VIDEODRIVER=wayland
-    # export CLUTTER_BACKEND=wayland
-    # export XDG_CURRENT_DESKTOP=Hyprland
-    # export XDG_SESSION_TYPE=wayland
-    # export XDG_SESSION_DESKTOP=Hyprland
-    # export QT_AUTO_SCREEN_SCALE_FACTOR=1
-    # export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-    # export XCURSOR_THEME=Adwaita
-    # export XCURSOR_SIZE=40
-
-    # NVIDIA
-    # export GBM_BACKEND=nvidia-drm
-    # export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    # export LIBVA_DRIVER_NAME=nvidia
-    # export __GL_GSYNC_ALLOWED=0
-    # export __GL_VRR_ALLOWED=0
-    # export WLR_DRM_NO_ATOMIC=1
-
     OS=$(cat /etc/os-release | grep -e "^NAME=" | sed s/NAME=//g)
-
     if [[ "$OS" == "NixOS" ]]
     then
-      ${pkgs.hyprland}/bin/Hyprland &> /dev/null # TODO: uncomment this once distro-independence is reached
-      # nixGL ${pkgs.hyprland}/bin/Hyprland &> /dev/null
+      ${pkgs.hyprland}/bin/Hyprland &> /dev/null
     else
       nixGL ${pkgs.hyprland}/bin/Hyprland &> /dev/null
     fi
