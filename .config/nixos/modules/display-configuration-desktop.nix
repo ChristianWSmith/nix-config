63465992f397fde0f6821f087398909764280fe5
Christian Smith <smith.christian.william@gmail.com>
2023-09-01 23:11:20 -0400

    fix eww bar workspaces on multimonitor

--- .config/nixos/modules/display-configuration-desktop.nix
+++ .config/nixos/modules/display-configuration-desktop.nix
@@ -6,8 +6,8 @@
         monitor=DP-1, 1920x1080@240, 1920x0, 1
         monitor=DP-3, 1920x1080, 3840x0, 1
 
-        workspace=11,monitor:DP-2
-        workspace=12,monitor:DP-3
+        workspace=8,monitor:DP-2
+        workspace=9,monitor:DP-3
         workspace=1,monitor:DP-1
         workspace=2,monitor:DP-1
         workspace=3,monitor:DP-1
@@ -15,9 +15,6 @@
         workspace=5,monitor:DP-1
         workspace=6,monitor:DP-1
         workspace=7,monitor:DP-1
-        workspace=8,monitor:DP-1
-        workspace=9,monitor:DP-1
-        workspace=10,monitor:DP-1
       '';
     };
   };
