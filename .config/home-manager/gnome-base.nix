8d818e594f7697ee2707a56eee17f91098bb9fac
Christian Smith <smith.christian.william@gmail.com>
2023-09-11 18:15:30 -0400

    binds for days

--- .config/home-manager/gnome-base.nix
+++ .config/home-manager/gnome-base.nix
@@ -49,12 +49,50 @@
     "org/gnome/desktop/search-providers" = {
       disabled = [
         "org.gnome.Nautilus.desktop"
-	"org.gnome.Calculator.desktop"
-	"org.gnome.Characters.desktop"
-	"org.gnome.clocks.desktop"
-	"org.gnome.seahorse.Application.desktop"
-	"org.gnome.Settings.desktop"
+        "org.gnome.Calculator.desktop"
+        "org.gnome.Characters.desktop"
+        "org.gnome.clocks.desktop"
+        "org.gnome.seahorse.Application.desktop"
+        "org.gnome.Settings.desktop"
       ];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-2 = [ "<Shift><Super>2" ];
+      move-to-workspace-3 = [ "<Shift><Super>3" ];
+      move-to-workspace-4 = [ "<Shift><Super>4" ];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      close = [ "<Shift><Super>q" ];
+    };
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      show-screenshot-ui = [ "<Super>s" ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      command = "kgx";
+      binding = "<Super>Return";
+      name = "Console";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      command = "nautilus";
+      binding = "<Super>e";
+      name = "Files";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      command = "firefox";
+      binding = "<Super>w";
+      name = "Browser";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      command = "gnome-system-monitor";
+      binding = "<Super>b";
+      name = "System Monitor";
     };
   };
 }
--- .config/home-manager/gnome-extensions.nix
+++ .config/home-manager/gnome-extensions.nix
@@ -8,12 +8,14 @@
     gnomeExtensions.tray-icons-reloaded
     gnomeExtensions.advanced-alttab-window-switcher
     gnomeExtensions.pano
+    gnomeExtensions.gtile
   ];
   dconf.settings = {
     "org/gnome/shell" = {
       disable-user-extensions = false;
       enabled-extensions = [
         "just-perfection-desktop@just-perfection" 
+        "gTile@vibou"
         "blur-my-shell@aunetx" 
         "gnome-ui-tune@itstime.tech"
         "pano@elhan.io"
@@ -43,6 +45,63 @@
     };
     "org/gnome/shell/extensions/gnome-ui-tune" = {
       increase-thumbnails-size = "300%";
+    };
+    "org/gnome/shell/extensions/gtile" = {
+      grid-sizes = "2x2";
+      show-icon = false;
+      auto-close-keyboard-shortcut = true;
+      show-toggle-tiling = [ "<Super>t" ];
+      show-toggle-tiling-alt = [ "" ];
+      autotile-main = [ "" ];
+      autotile-1 = [ "" ];
+      autotile-2 = [ "" ];
+      autotile-3 = [ "" ];
+      autotile-4 = [ "" ];
+      autotile-5 = [ "" ];
+      autotile-6 = [ "" ];
+      autotile-7 = [ "" ];
+      autotile-8 = [ "" ];
+      autotile-9 = [ "" ];
+      autotile-10 = [ "" ];
+      snap-to-neighbors = [ "" ];
+      preset-resize-1 = [ "" ];
+      preset-resize-2 = [ "" ];
+      preset-resize-3 = [ "" ];
+      preset-resize-4 = [ "" ];
+      preset-resize-5 = [ "" ];
+      preset-resize-6 = [ "" ];
+      preset-resize-7 = [ "" ];
+      preset-resize-8 = [ "" ];
+      preset-resize-9 = [ "" ];
+      preset-resize-10 = [ "" ];
+      preset-resize-11 = [ "" ];
+      preset-resize-12 = [ "" ];
+      preset-resize-13 = [ "" ];
+      preset-resize-14 = [ "" ];
+      preset-resize-15 = [ "" ];
+      preset-resize-16 = [ "" ];
+      preset-resize-17 = [ "" ];
+      preset-resize-18 = [ "" ];
+      preset-resize-19 = [ "" ];
+      preset-resize-20 = [ "" ];
+      preset-resize-21 = [ "" ];
+      preset-resize-22 = [ "" ];
+      preset-resize-23 = [ "" ];
+      preset-resize-24 = [ "" ];
+      preset-resize-25 = [ "" ];
+      preset-resize-26 = [ "" ];
+      preset-resize-27 = [ "" ];
+      preset-resize-28 = [ "" ];
+      preset-resize-29 = [ "" ];
+      preset-resize-30 = [ "" ];
+      move-left-vi = [ "" ];
+      move-right-vi = [ "" ];
+      move-up-vi = [ "" ];
+      move-down-vi = [ "" ];
+      resize-left-vi = [ "" ];
+      resize-right-vi = [ "" ];
+      resize-up-vi = [ "" ];
+      resize-down-vi = [ "" ];
     };
   };
 }
