a1f6ba240e01de52ec497bca57bf2773af687bb4
Christian Smith <smith.christian.william@gmail.com>
2023-09-17 19:45:55 -0400

    theme foot

--- .config/home-manager/features/terminal.nix
+++ .config/home-manager/features/terminal.nix
@@ -19,8 +19,24 @@ in
         font = "${theme.monoFontName}:pixelsize=${builtins.toString theme.fontSizeUI}";
       };
       colors = {
-        alpha=theme.colorScheme.transparencyBackgroundRGB;
-        background=theme.colorScheme.background1Hex;
+        alpha = theme.colorScheme.transparencyBackgroundRGB;
+        background = theme.colorScheme.background1Hex;
+        regular0 = theme.colorScheme.background1Hex;
+        regular1 = theme.colorScheme.redHex;
+        regular2 = theme.colorScheme.greenHex;
+        regular3 = theme.colorScheme.yellowHex;
+        regular4 = theme.colorScheme.blueHex;
+        regular5 = theme.colorScheme.magentaHex;
+        regular6 = theme.colorScheme.cyanHex;
+        regular7 = theme.colorScheme.foreground4Hex;
+        bright0 = theme.colorScheme.background4Hex;
+        bright1 = theme.colorScheme.redHex;
+        bright2 = theme.colorScheme.greenHex;
+        bright3 = theme.colorScheme.yellowHex;
+        bright4 = theme.colorScheme.blueHex;
+        bright5 = theme.colorScheme.magentaHex;
+        bright6 = theme.colorScheme.cyanHex;
+        bright7 = theme.colorScheme.foreground1Hex;
       };
     };
   };
