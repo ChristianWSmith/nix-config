500affd41cdeefa101d78a69a3d900e44e5e7cf7
Christian Smith <smith.christian.william@gmail.com>
2023-09-18 10:45:43 -0400

    move hypr

--- .config/home-manager/features/hyprland.nix
+++ .config/home-manager/features/hyprland.nix
@@ -74,6 +74,98 @@ in
 {
   home.file = {
     ".tty1-gui-only".text = "";
+    ".config/hypr/autoexec.conf".text = ''
+      exec-once = foot --server
+      exec-once = hyprpaper
+      exec-once = waybar
+      exec-once = hyprland-enable-screen-sharing
+      exec-once = wl-paste --watch cliphist store
+      exec-once = mkdir -p $HOME/Pictures $HOME/Documents $HOME/Videos $HOME/Music $HOME/Downloads $HOME/Templates $HOME/Desktop
+    '';
+    ".config/hypr/binds.conf".text = ''
+      $mainMod = SUPER
+      bind = $mainMod, RETURN, exec, terminal
+      bind = $mainMod, W, exec, $BROWSER
+      bind = $mainMod, E, exec, nemo
+      bind = $mainMod, S, exec, hyprland-screenshot
+      bind = $mainMod, P, exec, hyprland-colorpicker
+      bind = $mainMod, A, exec, terminal pulsemixer
+      bind = $mainMod, D, exec, app-launcher
+      bind = $mainMod, T, exec, system-tray
+      bind = $mainMod, B, exec, terminal btop
+      bind = $mainMod, Z, exec, zen-toggle
+      bind = $mainMod, O, exec, emoji type
+
+      bind = $mainMod SHIFT, Q, killactive,
+      bind = $mainMod SHIFT, E, exec, power-menu
+      bind = $mainMod, SPACE, togglefloating,
+      bind = $mainMod, F, fullscreen,
+
+      bind = $mainMod, J, movefocus, l
+      bind = $mainMod, L, movefocus, r
+      bind = $mainMod, I, movefocus, u
+      bind = $mainMod, K, movefocus, d
+
+      bind = $mainMod SHIFT, J, movewindow, l
+      bind = $mainMod SHIFT, L, movewindow, r
+      bind = $mainMod SHIFT, I, movewindow, u
+      bind = $mainMod SHIFT, K, movewindow, d
+
+      bindm = $mainMod, mouse:272, movewindow
+      bindm = $mainMod, mouse:273, resizewindow
+
+      bind = $mainMod, 1, workspace, 1
+      bind = $mainMod, 2, workspace, 2
+      bind = $mainMod, 3, workspace, 3
+      bind = $mainMod, 4, workspace, 4
+      bind = $mainMod, 5, workspace, 5
+      bind = $mainMod, 6, workspace, 6
+      bind = $mainMod, 7, workspace, 7
+      bind = $mainMod, 8, workspace, 8
+      bind = $mainMod, 9, workspace, 9
+      bind = $mainMod, 0, workspace, 10
+
+      bind = $mainMod SHIFT, 1, movetoworkspace, 1
+      bind = $mainMod SHIFT, 2, movetoworkspace, 2
+      bind = $mainMod SHIFT, 3, movetoworkspace, 3
+      bind = $mainMod SHIFT, 4, movetoworkspace, 4
+      bind = $mainMod SHIFT, 5, movetoworkspace, 5
+      bind = $mainMod SHIFT, 6, movetoworkspace, 6
+      bind = $mainMod SHIFT, 7, movetoworkspace, 7
+      bind = $mainMod SHIFT, 8, movetoworkspace, 8
+      bind = $mainMod SHIFT, 9, movetoworkspace, 9
+      bind = $mainMod SHIFT, 0, movetoworkspace, 10
+    '';
+    ".config/hypr/environment.conf".text = ''
+
+    '';
+    ".config/hypr/input.conf".text = ''
+      input {
+          kb_layout = us
+          kb_variant =
+          kb_model =
+          kb_options =
+          kb_rules =
+          follow_mouse = 1
+          touchpad {
+              natural_scroll = true
+          }
+          sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+      }
+      gestures {
+          workspace_swipe = false
+      }
+      device:epic-mouse-v1 {
+          sensitivity = -0.5
+      }
+    '';
+    ".config/hypr/windowrules.conf".text = ''
+      windowrulev2 = move 100% 100%,class:(trayer),title:(panel)
+      windowrulev2 = float,class:(trayer),title:(panel)
+      windowrulev2 = pin,class:(trayer),title:(panel)
+      windowrulev2 = float,class:(wlogout),title:(wlogout)
+      windowrulev2 = noanim,class:(wlogout),title:(wlogout)
+    '';
     ".config/hypr/appearance.conf".text = ''
       blurls=waybar
       layerrule=ignorezero, waybar
