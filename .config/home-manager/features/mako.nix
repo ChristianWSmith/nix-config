7ce4054671251752180b13ece40ca34d682c9b5a
Christian Smith <smith.christian.william@gmail.com>
2023-09-20 08:36:24 -0400

    mako foreground

--- .config/home-manager/features/mako.nix
+++ .config/home-manager/features/mako.nix
@@ -5,6 +5,8 @@
     anchor = "top-center";
     backgroundColor = "#${theme.colorScheme.background1Hex}${theme.colorScheme.transparencyBackgroundHex}";
     borderColor = "#${theme.colorScheme.accentHex}${theme.colorScheme.transparencyForegroundHex}";
+    textColor = "#${theme.colorScheme.foreground1Hex}";
+    progressColor = "#${theme.colorScheme.accentHex}";
     borderSize = theme.borderWidth;
     borderRadius = theme.borderRadius;
     defaultTimeout = 3000;
